ddc61753dd39652448ab82912c0f94cb39956845
gotojo <MXI03066@gmail.com>
2014-03-08 14:02:40 +0900

    .

---
+++ imadokoplayer/SCPlayer.h
@@ -0,0 +1,13 @@
+//
+//  SCPlayer.h
+//  imadokoplayer
+//
+//  Created by gotojo on 2014/03/08.
+//  Copyright (c) 2014年 imadokoplayerteam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCPlayer : NSObject
+
+@end
---
+++ imadokoplayer/SCPlayer.m
@@ -0,0 +1,13 @@
+//
+//  SCPlayer.m
+//  imadokoplayer
+//
+//  Created by gotojo on 2014/03/08.
+//  Copyright (c) 2014年 imadokoplayerteam. All rights reserved.
+//
+
+#import "SCPlayer.h"
+
+@implementation SCPlayer
+
+@end
