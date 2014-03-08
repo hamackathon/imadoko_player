f017d4f3d9eb2317da3897eb8741c4dcadeba82d
Masakuni Kato <kato@airs.co.jp>
2014-03-08 15:08:55 +0900

    add timer function to view controller

--- imadokoplayer/ViewController.m
+++ imadokoplayer/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *hourLabel;
+@property (weak, nonatomic) IBOutlet UILabel *minuteLabel;
+
 @end
 
 @implementation ViewController
@@ -20,10 +23,41 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    // タイマー
+    [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(didSecondUpdate:) userInfo:nil repeats:YES];
+    
+    // 位置
+    
+    
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)didSecondUpdate:(NSTimer *)timer
+{
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *comps = [[NSDateComponents alloc] init];
+    [comps setYear:2014];
+    [comps setMonth:3];
+    [comps setDay:8];
+    [comps setHour:19];
+    [comps setMinute:0];
+    [comps setSecond:0];
+    NSDate *target = [calendar dateFromComponents:comps];
+    NSDate *now = [NSDate date];
+    
+    float tmp= [target timeIntervalSinceDate:now];
+    int hh = (int)(tmp / 3600);
+    int mm = (int)((tmp - (hh * 3600)) / 60);
+    
+    self.hourLabel.text = [NSString stringWithFormat:@"%02d", hh];
+    self.minuteLabel.text = [NSString stringWithFormat:@"%02d", mm];
 }
 
 @end
